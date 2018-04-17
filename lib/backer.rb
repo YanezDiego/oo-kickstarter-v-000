cc72628e2ced46eba939adaa4833ad97fba310ba
Diego Yanez <diegosyanez92@gmail.com>
2018-04-17 20:18:31 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,14 +3,14 @@ require 'pry'
 class Backer
   attr_accessor :name, :title, :project
 
-@backed_projects = []
+
 
     def initialize(name)
       @name = name
     end
 
     def back_project(project)
-      self.backed_projects << project
+
     end
 
 
