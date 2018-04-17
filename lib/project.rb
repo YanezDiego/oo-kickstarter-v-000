ef6e6a844069bbef19a37a78cf9802c24bb2d76e
Diego Yanez <diegosyanez92@gmail.com>
2018-04-17 20:32:02 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,11 +6,11 @@ class Project
 
     def initialize(title)
       @title = title
-      @backers = []
+      @backer = []
     end
 
     def add_backer(backer)
-      @backers << backer
+      @backer << backer
     end
 
 
